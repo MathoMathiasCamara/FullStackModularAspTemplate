8b8d5693ddfddfd86ee6039ec6973c7567206178
matho mathias camara <matho.camara@gmail.com>
2023-05-31 21:36:02 +0200

    updated

---
+++ Asp.React/Pages/React.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Asp.React.MyFeature.Pages
+{
+    public class ReactModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
